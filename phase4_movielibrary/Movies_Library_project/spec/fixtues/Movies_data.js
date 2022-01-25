14de06d90a9d88ccf8cfd375e999d5fdc98cd924
sharath-zeomega <94888612+sharath-zeomega@users.noreply.github.com>
2022-01-25 10:54:56 +0530

    Add files via upload

---
+++ phase4_movielibrary/Movies_Library_project/spec/fixtues/Movies_data.js
@@ -0,0 +1,32 @@
+
+
+//restorents information are stored in this file using Fixtures
+var FIXTURES = window.FIXTURES || {};
+
+FIXTURES.movies ={
+
+    movie1:{
+        name:"Rajvishnu",
+        rating:"4.5",
+        genres:"comedy",
+        comment:"very Good"
+    },
+    movie2:{
+        name:"spider man no way home",
+        rating:"4.8",
+        genres:"adventure",
+        comment:"excelent movie"
+    },
+    movie3:{
+        name:"Jaggu",
+        rating:"4.3",
+        genres:"action",
+        comment:"Good"
+    },
+    movie4:{
+        name:"Aana",
+        rating:"4.5",
+        genres:"horror",
+        comment:"Good"
+    },
+}
